6fdbf28003393d54e6e858fbfd821eb0e8dd0e86
VariableV <student.mhuss@gmail.com>
2020-03-24 12:58:10 -0500

    generatefizz improvement

--- src/app/fizzbuzz/fizzbuzz.component.ts
+++ src/app/fizzbuzz/fizzbuzz.component.ts
@@ -16,16 +16,14 @@ export class FizzbuzzComponent implements OnInit {
   generateFizz(n : number): void{
     for(let i = 1; i <= n; i++){
       let str = "";
-      if (i%3 == 0 || i%5 == 0){
-        if (i%3 == 0){
-          str += "Fizz";
-        }
-        if (i%5 == 0){
-          str += "Buzz";
-        }
+      if (i % 3 == 0){
+        str += "Fizz";
       }
-      else{
-        str = i+"";
+      if (i % 5 == 0){
+        str += "Buzz";
+      }
+      if (str.length == 0){
+        str = ""+i;
       }
       this.buzz.push(str);
     }
